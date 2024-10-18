3a656b0de4840495123876e070851caf9fc74e69
Silfur <solargente1508@gmail.com>
2024-10-18 11:46:11 +0200

    no more fzf for now but telescope works fine without it

--- lua/silfur/plugins/telescope.lua
+++ lua/silfur/plugins/telescope.lua
@@ -1,59 +1,30 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  tag = "0.1.5",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-telescope/telescope-ui-select.nvim",
-    {
-      "nvim-telescope/telescope-fzf-native.nvim",
-      build = "make",
-      enabled = vim.fn.executable("make") == 1,
+    "nvim-telescope/telescope.nvim",
+
+    tag = "0.1.5",
+
+    dependencies = {
+        "nvim-lua/plenary.nvim"
     },
-  },
-  ft = 'mason',
-  keys = function()
-    local builtin = require("telescope.builtin")
-    return {
-      --- Search
-      { "<leader>sg", builtin.live_grep,           desc = "Live search" },
-      {
-        "<leader>sw",
-        function()
-          builtin.grep_string({ word_match = "-w" })
-        end,
-        desc = "Grep a Word"
-      },
-      --- Find
-      { "<leader>fd", builtin.lsp_definitions,     desc = "Find Definitions" },
-      { "<leader>ff", builtin.find_files,          desc = "Find Files" },
-      { "<leader>fr", builtin.lsp_references,      desc = "Find References" },
-      { "<leader>fi", builtin.lsp_implementations, desc = "Find Implementations" },
-      --- Other
-      {
-        "<leader>N",
-        function()
-          require('telescope').extensions.notify.notify()
-        end,
-        desc = "Last notifications"
-      }
-    }
-  end,
-  config = function()
-    require("telescope").setup({
-      extensions = {
-        ["ui-select"] = {
-          require("telescope.themes").get_dropdown({}),
-        },
-        fzf = {
-          fuzzy = true,
-          override_generic_sorter = true,
-          override_file_sorter = true,
-          case_mode = "smart_case",
-        }
-      },
-    })
-    require('telescope').load_extension('fzf')
-    require("telescope").load_extension("ui-select")
-    require("telescope").load_extension("notify")
-  end,
+
+    config = function()
+        require('telescope').setup({})
+
+        local builtin = require('telescope.builtin')
+        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+        vim.keymap.set('n', '<leader>pws', function()
+            local word = vim.fn.expand("<cword>")
+            builtin.grep_string({ search = word })
+        end)
+        vim.keymap.set('n', '<leader>pWs', function()
+            local word = vim.fn.expand("<cWORD>")
+            builtin.grep_string({ search = word })
+        end)
+        vim.keymap.set('n', '<leader>ps', function()
+            builtin.grep_string({ search = vim.fn.input("Grep > ") })
+        end)
+        vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+    end
 }
+
